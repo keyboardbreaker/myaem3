12862aee5292ae5e1f0b998c0c1310055b162ff6
keyboardbreaker <yujin_1991@hotmail.com>
2026-03-03 22:55:18 +0000

    2 column promo section

--- ui.frontend/src/index.js
+++ ui.frontend/src/index.js
@@ -13,7 +13,7 @@ import './components/import-components';
 import './index.css';
 import './styles/hero.css';
 import './styles/layout.css';
-
+import './styles/orange-promo.css';
 const modelManagerOptions = {};
 if(process.env.REACT_APP_PROXY_ENABLED) {
     modelManagerOptions.modelClient = new LocalDevModelClient(process.env.REACT_APP_API_HOST);
